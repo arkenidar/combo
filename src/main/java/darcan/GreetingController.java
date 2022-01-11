c0270a901e6e02e85fdcd374035dc44f3360ecab
Dario Cangialosi Arkenidar <dario.cangialosi@gmail.com>
2022-01-11 13:10:18 +0100

    autoclean

--- src/main/java/darcan/GreetingController.java
+++ src/main/java/darcan/GreetingController.java
@@ -3,7 +3,6 @@ package darcan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,17 +14,17 @@ public class GreetingController {
 		return "greeting";
 	}
 
-    @GetMapping("/ajax")
-    public @ResponseBody String ajax(){
-        return "[11,22,33,\"from server!\"]";
-    }
+	@GetMapping("/ajax")
+	public @ResponseBody String ajax() {
+		return "[11,22,33,\"from server!\"]";
+	}
 
 	@GetMapping("/ajax2")
-    public @ResponseBody String[] ajax2(){
+	public @ResponseBody String[] ajax2() {
 		// you can also use an "object"/"java bean"
 		// and the data will be converted to JSON
-		String[] list={"first","second","third","from Server via JSON from data"};
-        return list;
-    }
+		String[] list = { "first", "second", "third", "from Server via JSON from data" };
+		return list;
+	}
 
 }
