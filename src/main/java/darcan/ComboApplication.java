c3fdc535e3f2ec14ecf8b98e3eb8cc531b9dedac
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-01-11 09:18:22 +0100

    first commit

---
+++ src/main/java/darcan/ComboApplication.java
@@ -0,0 +1,13 @@
+package darcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComboApplication.class, args);
+	}
+
+}
---
+++ src/main/java/darcan/GreetingController.java
@@ -0,0 +1,31 @@
+package darcan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("/")
+	public String greeting(Model model) {
+		model.addAttribute("name", "Server");
+		return "greeting";
+	}
+
+    @GetMapping("/ajax")
+    public @ResponseBody String ajax(){
+        return "[11,22,33,\"from server!\"]";
+    }
+
+	@GetMapping("/ajax2")
+    public @ResponseBody String[] ajax2(){
+		// you can also use an "object"/"java bean"
+		// and the data will be converted to JSON
+		String[] list={"first","second","third","from Server via JSON from data"};
+        return list;
+    }
+
+}
